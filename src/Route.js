39633735bd1011a5e45364e803bd02ed8498b125
nar789 <admin@hume.co.kr>
2021-09-24 03:35:43 +0900

    init

---
+++ src/Route.js
@@ -0,0 +1,35 @@
+;(function(){
+	module.exports=function(_g){
+
+		var app = _g.app;
+		var http = _g.http;
+
+		function route(){
+			app.get('/',function(req,res){
+				res.render('index.html',{});
+			});
+			app.get('/chat',function(req,res){
+				res.render('chat.html',{});
+			});
+
+			//1. enetry point
+			app.listen(9924,function(){
+			  preLoad();
+			  console.log('rrr# Server listen on *:9924');
+			});
+		}
+
+		function preLoad(){
+			//to-do
+		}
+
+		var publicReturn = {
+			route:route,
+		}
+		return publicReturn;
+	}
+
+})();
+
+
+
